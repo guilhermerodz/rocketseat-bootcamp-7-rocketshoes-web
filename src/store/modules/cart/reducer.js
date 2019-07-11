3e87c391d3c336d6f42babf917ef221b50cde3f8
guilhermerodz <guilherme.rodrigues.profissional@gmail.com>
2019-07-11 19:03:01 -0300

    feat: remove product action

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -9,6 +9,12 @@ export default function cart(state = [], action) {
         if (productIndex >= 0) draft[productIndex].amount += 1;
         else draft.push({ ...action.product, amount: 1 });
       });
+    case 'REMOVE_FROM_CART':
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(p => p.id === action.id);
+
+        if (productIndex >= 0) draft.splice(productIndex, 1);
+      });
     default:
       return state;
   }
