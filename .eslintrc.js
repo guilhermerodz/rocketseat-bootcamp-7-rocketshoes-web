b9099a294871cec715857b4064e3018407cf87b2
guilhermerodz <guilherme.rodrigues.profissional@gmail.com>
2019-07-11 18:54:51 -0300

    chore: now cart handles duplicated items

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,6 @@ module.exports = {
     'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
     'import/prefer-default-export': 'off',
     'no-console': ['error', { allow: ['tron'] }],
+    'no-param-reassign': 'off',
   },
 };
