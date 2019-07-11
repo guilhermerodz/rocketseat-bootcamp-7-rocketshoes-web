42670cab3a03b10db83831243a8b44f85adeab2d
guilhermerodz <guilherme.rodrigues.profissional@gmail.com>
2019-07-11 12:53:32 -0300

    feat: added Header

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './styles/global';
 import Routes from './routes';
 
+import Header from './components/Header';
+
 function App() {
   return (
     <BrowserRouter>
-      {/* <Header /> */}
+      <Header />
       <Routes />
       <GlobalStyle />
     </BrowserRouter>
