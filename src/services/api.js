31647b15263d4b42e330eaf52593c48d07ef8a0e
guilhermerodz <guilherme.rodrigues.profissional@gmail.com>
2019-07-14 13:57:21 -0300

    chore: back-end api is now externally hosted

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL:
+    'https://my-json-server.typicode.com/guilhermerodz/rocketshoes-fake-api',
 });
 
 export default api;
